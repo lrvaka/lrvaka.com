df0fbd6456c816bdd9723941d6af067faf5c20fb
lrvaka <lukevakasisikakala@gmail.com>
2023-02-01 18:58:58 -0500

    add umami to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,13 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${inter.style.fontFamily};
         }
       `}</style>
-
+      {/* Umami Analytics */}
+      <script
+        async
+        defer
+        data-website-id="5508f933-9bbf-4bfa-962c-db9b69428f34"
+        src="https://umami-lrvaka-com.vercel.app/umami.js"
+      ></script>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,7 @@ import { GA_TRACKING_ID } from "../lib/gtag";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Umami Analytics */}
-        <script
-          async
-          defer
-          data-website-id="5508f933-9bbf-4bfa-962c-db9b69428f34"
-          src="https://umami-lrvaka-com.vercel.app/umami.js"
-        ></script>
-      </Head>
+
       <body className=" bg-gray-100 text-black dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
