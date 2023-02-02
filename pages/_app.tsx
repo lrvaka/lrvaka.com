c8924105efc9ccc43b091ad5e418f0d944adbd18
lrvaka <lukevakasisikakala@gmail.com>
2023-02-01 19:00:22 -0500

    script component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "@next/font/google";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       {/* Umami Analytics */}
-      <script
+      <Script
         async
         defer
         data-website-id="5508f933-9bbf-4bfa-962c-db9b69428f34"
         src="https://umami-lrvaka-com.vercel.app/umami.js"
-      ></script>
+      />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
       </ThemeProvider>
