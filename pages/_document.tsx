3717de665d4cec46d987c23b5cad11f95f944ee7
lrvaka <lukevakasisikakala@gmail.com>
2023-02-01 19:08:41 -0500

    add head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function Document() {
   return (
     <Html lang="en">
-
+      <Head></Head>
       <body className=" bg-gray-100 text-black dark:bg-gray-900 dark:text-white">
         <Main />
         <NextScript />
